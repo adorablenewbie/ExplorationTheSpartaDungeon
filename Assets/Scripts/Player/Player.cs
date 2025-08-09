c10c140eebd6b573e86bc99e81f8dd9dc0ea0c38
adorablenewbie <youngjong2965@gmail.com>
2025-08-09 13:03:58 +0900

    Initial commit

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerController controller;
+    public PlayerCondition condition;
+
+    public ItemData itemData;
+    public Action addItem;
+
+    public Equipment equip;
+
+    public Transform dropPosition;
+
+    private void Awake()
+    {
+        CharacterManager.Instance.Player = this;
+        controller = GetComponent<PlayerController>();
+        condition = GetComponent<PlayerCondition>();
+        equip = GetComponent<Equipment>();
+    }
+}
