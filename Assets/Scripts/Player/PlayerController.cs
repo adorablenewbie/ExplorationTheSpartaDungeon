512086d6be365fab75a4ce998b164a42a3b3588b
adorablenewbie <youngjong2965@gmail.com>
2025-08-11 23:01:28 +0900

    [ fix ] 카메라 앵글 무빙 픽스

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -94,8 +94,6 @@ public class PlayerController : MonoBehaviour
         camCurXRot += mouseDelta.y * lookSensitivity;
         camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
         cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
-
-        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
     }
 
     private void RotateToCamera()
